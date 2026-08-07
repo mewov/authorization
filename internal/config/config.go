78de1fb4809d0dc154fffa913c4882064dbb0a88
mewov <stowhh@gmail.com>
2026-08-07 22:53:28 +0300

    refactor: remove getters from config

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,27 +12,3 @@ type (
 		PostgresUrl  string
 	}
 )
-
-func (c *Config) GetAddress() string {
-	return c.Address
-}
-
-func (c *Config) GetRestPort() string {
-	return c.RestPort
-}
-
-func (c *Config) GetGrpcPort() string {
-	return c.GrpcPort
-}
-
-func (c *Config) GetWriteTimeout() time.Duration {
-	return c.WriteTimeout
-}
-
-func (c *Config) GetReadTimeout() time.Duration {
-	return c.ReadTimeout
-}
-
-func (c *Config) GetPostgresUrl() string {
-	return c.PostgresUrl
-}
